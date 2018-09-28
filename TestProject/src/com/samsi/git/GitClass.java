ec1b5906d0bdd664a38b44b642d76aa841ec9c4f
Sangu <Sangu@Windows-10>
2018-09-29 01:39:17 +0530

    New GitClass Added

---
+++ TestProject/src/com/samsi/git/GitClass.java
@@ -0,0 +1,10 @@
+package com.samsi.git;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Welcome to Git Version controll");
+	}
+
+}
